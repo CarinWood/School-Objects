32ad2430b39b622bd4906b1e4016c1383c380a48
CarinWood <carin.wood.85@gmail.com>
2024-01-15 15:31:33 +0100

    update

--- index.js
+++ index.js
@@ -4,43 +4,52 @@ let school = {
     zipCode: 12498,
     city: "Gothenburg",
     students: [],
-    teachers: []
+    teachers: [],
 }
 
 let math = {
     name: 'Math',
     students: [],
-    teachers: {},
+    teacher: {},
     addTeacher(teacher) {
-        this.teachers = teacher
+        this.teacher = teacher
     },
     addStudent(student) {
         this.students.push(student.name)
+    },
+    removeTeacher(teacher) {
+       teacher = {}
     }
 }
 
 let science = {
     name: 'Science',
     students: [],
-    teachers: {},
+    teacher: {},
     addTeacher(teacher) {
-        this.teachers = teacher
+        this.teacher = teacher
     },
     addStudent(student) {
         this.students.push(student.name)
-    }
+    },
+    removeTeacher(teacher) {
+        teacher = {}
+     }
 }
 
 let english = {
     name: 'English',
     students: [],
-    teachers: {},
+    teacher: {},
     addTeacher(teacher) {
-        this.teachers = teacher
+        this.teacher = teacher
     },
     addStudent(student) {
         this.students.push(student.name)
-    }
+    },
+    removeTeacher(teacher) {
+        teacher = {}
+     }
 }
 
 /* students */
@@ -52,6 +61,12 @@ let ben = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    quitSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
+        let nameIndex = english.students.indexOf(this.name)
+        english.students.splice(nameIndex, 1)
     }
 }
 
@@ -62,6 +77,12 @@ let joe = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    quitSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
+        let nameIndex = english.students.indexOf(this.name)
+        english.students.splice(nameIndex, 1)
     }
 }
 
@@ -72,6 +93,12 @@ let linda = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    quitSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
+        let nameIndex = english.students.indexOf(this.name)
+        english.students.splice(nameIndex, 1)
     }
 }
 
@@ -82,6 +109,12 @@ let susannah = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    quitSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
+        let nameIndex = english.students.indexOf(this.name)
+        english.students.splice(nameIndex, 1)
     }
 }
 
@@ -92,6 +125,12 @@ let maya = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    quitSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
+        let nameIndex = english.students.indexOf(this.name)
+        english.students.splice(nameIndex, 1)
     }
 }
 
@@ -102,6 +141,10 @@ let smith = {
     subjects: [],
     addSubject(subject) {
         this.subjects.push(subject.name)
+    },
+    removeSubject(subject) {
+        let index = this.subjects.indexOf(subject)
+        this.subjects.splice(index, 1)
     }
 }
 
@@ -130,6 +173,8 @@ function addSubjectToTeacher(subject, teacher) {
 
 console.log(addSubjectToTeacher(english, gray))
 
+
+
 /* provar lärarens funktion: */
 
 smith.addSubject(science)
@@ -138,10 +183,13 @@ console.log(smith.subjects)
 /* provar de olika funktionerna: */
 
 math.addTeacher(smith)
-console.log(math.teachers)
+console.log(math.teacher)
 
 english.addTeacher(gray)
-console.log(english.teachers)
+console.log(english.teacher)
+
+science.addTeacher(smith)
+console.log(science.teacher)
 
 math.addStudent(ben)
 math.addStudent(joe)
@@ -184,3 +232,54 @@ console.log("Maya's subjects: " + maya.subjects)
 
 console.log("Mr. Gray's subjects: " + gray.subjects)
 console.log("Mr. Smith's subjects: " + smith.subjects)
+
+
+/* quitSubject, removeTeacher, relegateStudent, fireTeacher */
+
+ben.quitSubject(math)
+console.log(ben.subjects)
+console.log(english.students)
+
+math.removeTeacher(smith)
+console.log(math.teacher)
+console.log(smith.subjects)
+smith.removeSubject("math")
+console.log(smith.subjects)
+
+
+school.students.push(maya)
+school.students.push(ben)
+school.students.push(susannah)
+school.students.push(joe)
+school.students.push(linda)
+for(student in school.students) {
+    console.log(school.students[student].name)
+}
+
+function relegateStudent(student) {
+    let index = school.students.indexOf(student)
+    school.students.splice(index, 1)
+ }
+
+
+relegateStudent(maya)
+console.log('******************')
+ for (student in school.students) {
+     console.log(school.students[student].name)
+ }
+
+ school.teachers.push(smith)
+ school.teachers.push(gray)
+
+
+
+ function fireTeacher(teacher) {
+    let index = school.teachers.indexOf(teacher)
+    school.teachers.splice(index, 1)
+ }
+
+ fireTeacher(smith)
+
+ for (teacher in school.teachers) {
+   console.log(school.teachers[teacher].name)
+ }
